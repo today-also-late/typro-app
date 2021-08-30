674e91d199eb6fa7972824f7af4705f5bed8f6fe
haruta8 <haruta8@jxpress.net>
2021-08-31 02:20:37 +0900

    style: ヘッダーのスタイル調整

--- src/components/atoms/TyproLogo.tsx
+++ src/components/atoms/TyproLogo.tsx
@@ -1,15 +1,18 @@
 import TyproLogoImage from "../../../public/images/typro-logo.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const TyproLogo = () => {
   return (
-    <Image
-      className="rounded-full"
-      src={TyproLogoImage}
-      alt="TyproLogoImage"
-      width={48}
-      height={48}
-    />
+    <Link href="/">
+      <Image
+        className="rounded-full"
+        src={TyproLogoImage}
+        alt="TyproLogoImage"
+        width={48}
+        height={48}
+      />
+    </Link>
   );
 };
 export default TyproLogo;
