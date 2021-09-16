348af0b6efdcfe0afb6b2f2038e6d833270ddfda
haruta8 <haruta8@jxpress.net>
2021-09-16 16:51:12 +0900

    fix:軽い調整

--- src/pages/users/selectlanguage.tsx
+++ src/pages/users/selectlanguage.tsx
@@ -19,31 +19,56 @@ const goSelectLevel = (selectedLanguage: string) => {
 const Selectlanguage = () => {
   return (
     <div className="">
-      <div className="text-center py-24 text-2xl">
+      <div className="text-center py-24 text-2xl font-bold">
         <p>言語を選択してください</p>
       </div>
-      <div className="flex justify-evenly">
-        <div>
-          <PythonLogo />
-          <PrimaryButton
-            label={"Python"}
-            onClick={() => goSelectLevel("python")}
-          />
+      <div className="grid grid-cols-2 xl:grid-cols-4 text-center">
+        <div className="bg-gray-200 rounded-lg shadow-xl overflow-hidden py-8 mr-4 ml-8">
+          <div className=" w-full object-cover">
+            <PythonLogo />
+          </div>
+          <div className="grid justify-items-center items-center text-center pt-8">
+            <PrimaryButton
+              label={"Python"}
+              onClick={() => goSelectLevel("python")}
+            />
+          </div>
         </div>
-        <div>
-          <JsLogo />
-          <PrimaryButton
-            label={"JavaScript"}
-            onClick={() => goSelectLevel("javascript")}
-          />
+
+        <div className="bg-gray-200 rounded-lg shadow-xl overflow-hidden py-8 mx-4">
+          <div className=" w-full object-cover">
+            <JsLogo />
+          </div>
+          <div className="grid justify-items-center items-center text-center pt-8">
+            <PrimaryButton
+              label={"JavaScript"}
+              onClick={() => goSelectLevel("javascript")}
+            />
+          </div>
         </div>
-        <div>
-          <CLogo />
-          <PrimaryButton label={"C"} onClick={() => goSelectLevel("python")} />
+
+        <div className="bg-gray-200 rounded-lg shadow-xl overflow-hidden py-8 mx-4">
+          <div className=" w-full object-cover">
+            <CLogo />
+          </div>
+          <div className="grid justify-items-center items-center text-center pt-8">
+            <PrimaryButton
+              label={"C"}
+              onClick={() => goSelectLevel("python")}
+            />
+          </div>
         </div>
-        <div>
-          <GoLogo />
-          <PrimaryButton label={"Go"} onClick={() => goSelectLevel("python")} />
+
+        <div className="bg-gray-200 rounded-lg shadow-xl overflow-hidden py-8 ml-4 mr-8">
+          <div className=" w-full object-cover">
+            <GoLogo />
+          </div>
+          <div className="grid justify-items-center items-center text-center pt-8">
+            <PrimaryButton
+              label={"Go"}
+              onClick={() => goSelectLevel("python")}
+            />
+          </div>
         </div>
       </div>
     </div>
