58993f6098d7e204f36a0faeec5b798c9fa00cb0
Rai <raimi.rarara@gmail.com>
2021-09-23 01:52:17 +0900

    feat: hover追加

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -8,7 +8,7 @@ type PROPS = {
 
 const Button: React.FC<PROPS> = (props) => {
   return (
-    <button className="border w-20 rounded-md font-bold h-8 shadow  text-white text-xs">
+    <button className="border w-20 rounded-md font-bold h-8 shadow  text-white text-xs hover:text-gray-600 hover:border-gray-600">
       <Link href={props.href}>{props.buttonText}</Link>
     </button>
   );
